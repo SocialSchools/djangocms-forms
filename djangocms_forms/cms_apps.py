36bf1417e8728608b945ebd448f4e81120177a60
Vinit Kumar <vinit.kumar@socialschools.nl>
2018-07-02 18:14:45 +0200

    change the way urls are declared in the app hooks
    http://docs.django-cms.org/en/latest/how_to/apphooks.html#the-basics-of-apphook-creation

--- djangocms_forms/cms_apps.py
+++ djangocms_forms/cms_apps.py
@@ -10,6 +10,9 @@ from cms.apphook_pool import apphook_pool
 
 class DjangoCMSFormsApphook(CMSApp):
     name = _('Forms')
-    urls = ['djangocms_forms.urls']
+
+    def get_urls(self, page=None, language=None, **kwargs):
+        return ['djangocms_forms.urls']
+
 
 apphook_pool.register(DjangoCMSFormsApphook)
